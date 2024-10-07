20d19b72cecdf52a10cee842681aad2ba386599f
Abhinash nv <abhinashnv1564@gmail.com>
2024-10-07 10:51:08 +0530

    sum of natural number

--- main.c
+++ main.c
@@ -2,38 +2,17 @@
 #include <stdlib.h>
 
 
-
 int main()
 {
-   double n1,n2;
-   char opselector;
+    int n1,ans=0;
 
-   printf("add your first number: ");
-   scanf("%lf",&n1);
+    printf("add your number: ");
+    scanf("%d",&n1);
 
-   printf("add your opselector:+,-,*,/: ");
-   scanf(" %c",&opselector);
+    for(int i=1;i<=n1;i++){
 
-   printf("add your second number: ");
-   scanf("%lf",&n2);
-
-
-   if(opselector=='+'){
-       printf("%lf+%lf=%lf",n1,n2,n1+n2);
-   }
-   else if(opselector=='-'){
-       printf("%lf-%lf=%lf",n1,n2,n1-n2);
-   }
-   else if(opselector=='*'){
-       printf("%lf*%lf=%lf",n1,n2,n1*n2);
-   }
-   else if(opselector=='/'){
-       printf("%lf/%lf=%lf",n1,n2,n1/n2);
-   }
-   else{
-       printf("error");
-   }
-
-
-    return 0;
+       ans=ans+i;
+    }
+    printf("%d",ans);
 }
+
