04b2a52d7b334b088c68d0e2c37c034a31091949
Abhinash nv <abhinashnv1564@gmail.com>
2024-10-07 10:52:04 +0530

    Fibonacci Series Program in C

--- main.c
+++ main.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 int main()
 {
-    int n1,ans=0;
+    int n1,ans;
+    long long first=0,second=1;
 
     printf("add your number: ");
     scanf("%d",&n1);
 
-    for(int i=1;i<=n1;i++){
+    for(int i=1;i<n1;i++){
 
-       ans=ans+i;
+    ans=first+second;
+    printf("%d\n",ans);
+    first=second;
+    second=ans;
+
     }
-    printf("%d",ans);
 }
-
