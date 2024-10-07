72a7c5dc66c3b05b62593070879f7c4cb5bc2d4a
Abhinash nv <abhinashnv1564@gmail.com>
2024-10-07 10:53:31 +0530

    check num is palindrome or not

--- main.c
+++ main.c
@@ -1,24 +1,27 @@
 
-
+#include <stdlib.h>
 #include <stdio.h>
 
 int main()
+
 {
-    int num,ans,i=2;
+    int num,reverse=0,reminder;
 
     printf("add your number: ");
     scanf("%d",&num);
+    int org=num;
 
+    while(num!=0){
+        reminder=num%10;
+        reverse=reverse*10+reminder;
+        num=num/10;
+    }
 
-   for(i=2;i<=num;i++){
-    ans=i%2;
+    if(org==reverse){
+        printf("%d is palindrome",reverse);
+    }
+    else
+        printf("%d is not a palindrome",reverse);
 
-
-   if(ans==0)
-     printf(" ");
-
-     else
-        printf("%d",i);
-   }
 
 }
