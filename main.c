ff233f2d3910cab9b936559be01cf575020531b1
Abhinash nv <abhinashnv1564@gmail.com>
2024-10-07 10:54:08 +0530

    A to Z printing

--- main.c
+++ main.c
@@ -1,27 +1,9 @@
-
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
-
 {
-    int num,reverse=0,reminder;
-
-    printf("add your number: ");
-    scanf("%d",&num);
-    int org=num;
-
-    while(num!=0){
-        reminder=num%10;
-        reverse=reverse*10+reminder;
-        num=num/10;
+    for(char letter='A';letter<='z';letter++){
+        printf("%c\n",letter);
     }
-
-    if(org==reverse){
-        printf("%d is palindrome",reverse);
-    }
-    else
-        printf("%d is not a palindrome",reverse);
-
-
 }
